70e275d7206ccf08793e8cd5cb2a3c45dacbbe8c
vanshajkapoor <kapurvanshaj@gmail.com>
2023-02-04 00:15:44 +0530

    complete star rating

--- star-rating/src/components/StarRating.js
+++ star-rating/src/components/StarRating.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 
 const StarRating = () => {
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
   const resetStarRating = () => setRating(0);
   return (
     <div className="star-widget">
       <ul className="star-container">
-        <li onClick={() => setRating(1)}>{rating >= 1 ? '★' : '☆'}</li>
-        <li onClick={() => setRating(2)}>{rating >= 2 ? '★' : '☆'}</li>
-        <li onClick={() => setRating(3)}>{rating >= 3 ? '★' : '☆'}</li>
-        <li onClick={() => setRating(4)}>{rating >= 4 ? '★' : '☆'}</li>
-        <li onClick={() => setRating(5)}>{rating >= 5 ? '★' : '☆'}</li>
+        <li onMouseOut={() => setHover(0)} onMouseOver={() => setHover(1)} onClick={() => setRating(1)}>
+          {hover > 0 ? (hover >= 1 ? '★' : '☆') : rating >= 1 ? '★' : '☆'}
+        </li>
+        <li onMouseOut={() => setHover(0)} onMouseOver={() => setHover(2)} onClick={() => setRating(2)}>
+          {hover > 0 ? (hover >= 2 ? '★' : '☆') : rating >= 2 ? '★' : '☆'}
+        </li>
+        <li onMouseOut={() => setHover(0)} onMouseOver={() => setHover(3)} onClick={() => setRating(3)}>
+          {hover > 0 ? (hover >= 3 ? '★' : '☆') : rating >= 3 ? '★' : '☆'}
+        </li>
+        <li onMouseOut={() => setHover(0)} onMouseOver={() => setHover(4)} onClick={() => setRating(4)}>
+          {hover > 0 ? (hover >= 4 ? '★' : '☆') : rating >= 4 ? '★' : '☆'}
+        </li>
+        <li onMouseOut={() => setHover(0)} onMouseOver={() => setHover(5)} onClick={() => setRating(5)}>
+          {hover > 0 ? (hover >= 5 ? '★' : '☆') : rating >= 5 ? '★' : '☆'}
+        </li>
       </ul>
       <div>Rating : {rating}/5 </div>
       {rating > 0 && (
